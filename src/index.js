4f222c7459f321ea119ffc2c4a3bf7dc61f00878
refresh88 <ouok88@gmail.com>
2023-02-05 15:09:52 +0900

    shoppy 첫 구조 잡기
    라우터 구조 잡기

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AllProducts from './pages/AllProducts';
+import Home from './pages/Home';
+import MyCart from './pages/MyCart';
+import NewProduct from './pages/NewProduct';
+import NotFound from './pages/NotFound';
+import ProductDetail from './pages/ProductDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/products', element: <AllProducts /> },
+      { path: '/products/new', element: <NewProduct /> },
+      { path: '/products/:id', element: <ProductDetail /> },
+      { path: '/carts', element: <MyCart /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
