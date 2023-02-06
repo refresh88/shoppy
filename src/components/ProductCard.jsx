6f200c0af2f6e087f2faff2940ac5904b0349400
refresh88 <115513248+refresh88@users.noreply.github.com>
2023-02-06 23:18:13 +0900

    home, product list
    홈과 제품 페이지에 사용될 제품 리스트 컴포넌트 만들기 및 css

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,13 +4,13 @@ export default function ProductCard({
   product: { id, image, title, category, price },
 }) {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <h3>{title}</h3>
+    <li className='rounded-lg shaddow-md overflow-hidden cursor-pointer'>
+      <img className='w-full' src={image} alt={title} />
+      <div className='mt-2 px-2 text-lg flex justify-between items-center'>
+        <h3 className='truncate'> {title}</h3>
         <p>{`${price}원`}</p>
       </div>
-      <p>{category}</p>
+      <p className='mb-2 px-2 text-gray-600'>{category}</p>
     </li>
   );
 }
